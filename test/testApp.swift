5ea7bdbacf489a81c9090fdf4a37bcdb6f75f3e2
RikkaCode <y1743515032@gmail.com>
2026-05-22 10:20:01 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Rikka Takanashi on 2026/5/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
